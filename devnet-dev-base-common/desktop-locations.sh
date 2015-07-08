3bb6432b98bd4ef6d8a21ad5e96b6758453a69e1
Nathan Sowatskey <nsowatsk@cisco.com>
2015-07-08 10:47:59 +0200

    Experimenting with desktop locations. Not quite there yet.

---
+++ devnet-dev-base-common/desktop-locations.sh
@@ -0,0 +1,16 @@
+# Set the locations of the icons for the items in the Desktop. 
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/Chrome.desktop/' 'metadata::nautilus-icon-position' '64,262'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/Eclipse-JEE.desktop/' 'metadata::nautilus-icon-position' '64,10'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/IntelliJ.desktop/' 'metadata::nautilus-icon-position' '64,102'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/PyCharm.desktop/' 'metadata::nautilus-icon-position' '64,342'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/README.txt/' 'metadata::nautilus-icon-position' '156,114'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/Terminal.desktop/' 'metadata::nautilus-icon-position' '64,402'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/VMMaestro.desktop/' 'metadata::nautilus-icon-position' '64,182'
+
+gvfs-set-attribute -t string '/home/cisco-devnet/Desktop/Wireshark.desktop/' 'metadata::nautilus-icon-position' '142,10'
